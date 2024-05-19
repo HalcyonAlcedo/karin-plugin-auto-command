3d77da5162b42187129342c2f8e555984a712eda
zyc404 <zyc404@gmail.com>
2024-05-19 22:39:46 +0800

    初次提交

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
-[Karin](https://github.com/karinjs/karin) 插件包模板仓库
+# 自动命令插件
 
 ---
 
-- fork 本仓库
-- 拉取fork后的仓库到本地
-- 修改 `package.json` 文件中的 `name` 和 `description` 字段
-- 将 `README.md` 文件中的内容替换为自己的插件说明。
+基于讯飞星火对自然消息进行处理自动生成命令消息
+可以通过向机器人发送`#？ 我想查看系统状态`触发`#状态`命令
 
-## 克隆仓库
+## 安装
 
 karin根目录执行以下命令克隆仓库到本地
 
 ```bash
-git clone https://github.com/karinjs/karin-plugin-template.git ./plugins/karin-plugin-template
+git clone https://github.com/HalcyonAlcedo/karin-plugin-auto-command.git ./plugins/karin-plugin-auto-command
 ```
 
-## 安装依赖
+## 配置
 
-```bash
-pnpm install --filter=karin-plugin-template
-```
-
+根据[星火API接入](https://xinghuo.xfyun.cn/sparkapi)将`AppId`、`APISecret`、`APIKey`填入配置文件
+领取免费试用的模型套餐
+根据领取的模型套餐类型设置`Domain`模型，默认为V3.0
 
 ## 开发调试
 
---
+++ apps/autoCommand.js
@@ -0,0 +1,120 @@
+import lodash from 'lodash'
+import { plugin, segment, Bot, KarinMessage } from '#Karin'
+import loader from '../../../lib/plugins/loader.js'
+import SparkClient from '../lib/spark/client.js'
+import Cfg from '../lib/config/config.js'
+
+export class AutoCommand extends plugin {
+  constructor() {
+    super({
+      /** 插件名称 */
+      name: 'AutoCommand',
+      /** 插件描述 */
+      dsc: '自动化命令',
+      event: 'message',
+      priority: 999999,
+      rule: [
+        {
+          /** 命令正则匹配 */
+          reg: '^#(\\?|？)',
+          /** 执行方法 */
+          fnc: 'autoCommand',
+          /** 权限 master,owner,admin,all  */
+          permission: 'all'
+        }
+      ]
+    })
+  }
+
+  async autoCommand() {
+    let msg = this.e.msg.replace(/^#(\\?|？)/, '')
+    if (msg === '')  return
+    let apps = lodash.cloneDeep(loader.Apps)
+    apps = apps.map(item => {
+      if (item.rule && Array.isArray(item.rule)) {
+        item.rule = item.rule.map(rule => {
+          if (rule.reg) {
+            rule.reg = rule.reg.toString()
+          }
+          return rule
+        })
+      }
+      return item
+    })
+    const instructions = {}
+    // 将指令按照 file.dir 分类
+    apps.forEach(command => {
+      const { file, name, rule } = command
+      if (!instructions[file.dir]) {
+        instructions[file.dir] = {}
+      }
+
+      // 添加子类和对应的规则
+      instructions[file.dir][name] = rule.map(r => {
+        return { reg: r.reg, example: r.reg.replace(/\//g, '').replace(/\^|\$/g, '') }
+      })
+    })
+
+    const client = new SparkClient({
+      Domain: Cfg.Spark.Domain,
+      AppId: Cfg.Spark.AppId,
+      APISecret: Cfg.Spark.APISecret,
+      APIKey: Cfg.Spark.APIKey,
+      Temperature: 0.1,
+      MaxTokens: 2048
+    })
+
+    const command = await client.sendMessage(msg, {
+      prompt: [
+        {
+          "role": "system",
+          "content": `你是一个命令生成工具，根据用户的要求生成从命令列表中生成命令，如果有多个可用命令，选择相关性最高的命令回复，确保回复内容可以被命令列表中的正则表达式匹配。
+          例如当我说“我想检查是否有系统更新”时你需要回复
+          #检查更新`
+        },
+        {
+          "role": "user",
+          "content": `
+                  命令列表
+                  ${JSON.stringify(instructions)}
+              `
+        }
+      ]
+    })
+    if (command) {
+      this.e.elements = this.e.elements.map(item => {
+        if (item.text === this.e.msg) {
+          return segment.text(command.text)
+        }
+        return item
+      })
+      this.e.msg = command.text
+      this.e.raw_message = command.text
+      const message = {
+        self_id: this.e.self_id,
+        user_id: this.e.user_id,
+        time: this.e.time,
+        message_id: this.e.message_id,
+        message_seq: this.e.message_seq,
+        sender: {
+          uid: this.e.sender.uid,
+          uin: this.e.sender.uin,
+          nick: this.e.sender.nick
+        },
+        elements: this.e.elements,
+        contact: this.e.contact,
+        group_id: this.e.group_id,
+      }
+      const e = new KarinMessage(message)
+      e.replyCallback = async (elements) => {
+        const { uid, uin } = this.e.bot.account
+        return Bot.sendMsg(
+          uid || uin,
+          this.e.contact,
+          elements
+        )
+      }
+      Bot.emit('message', e)
+    }
+  }
+}
