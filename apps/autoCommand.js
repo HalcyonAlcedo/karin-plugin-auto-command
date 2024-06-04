84be8e3fe36d2955b872acb0406b5ae3b46c24a6
zyc404 <zyc404@gmail.com>
2024-06-04 22:09:15 +0800

    添加#命令列表功能

--- apps/autoCommand.js
+++ apps/autoCommand.js
@@ -4,6 +4,7 @@ import loader from '../../../lib/plugins/loader.js'
 import SparkClient from '../lib/spark/client.js'
 import Cfg from '../lib/config/config.js'
 
+let tempCommandList
 export class AutoCommand extends plugin {
   constructor() {
     super({
@@ -19,6 +20,14 @@ export class AutoCommand extends plugin {
           reg: '^#(\\?|？)',
           /** 执行方法 */
           fnc: 'autoCommand',
+          /** 权限 master,owner,admin,all  */
+          permission: 'all'
+        },
+        {
+          /** 命令正则匹配 */
+          reg: '^#命令列表',
+          /** 执行方法 */
+          fnc: 'commandList',
           /** 权限 master,owner,admin,all  */
           permission: 'all'
         }
@@ -67,7 +76,7 @@ export class AutoCommand extends plugin {
     const command = await client.sendMessage(msg, {
       prompt: [
         {
-          "role": "system",
+          "role": Cfg.Spark.Domain === "general" ? "user" : "system",
           "content": `你是一个命令生成工具，根据用户的要求生成从命令列表中生成命令，如果有多个可用命令，选择相关性最高的命令回复，确保回复内容可以被命令列表中的正则表达式匹配。
           例如当我说“我想检查是否有系统更新”时你需要回复
           #检查更新`
@@ -115,6 +124,58 @@ export class AutoCommand extends plugin {
         )
       }
       Bot.emit('message', e)
+    }
+  }
+  async commandList() {
+    if (tempCommandList) {
+      this.reply(tempCommandList)
+      return
+    }
+    let apps = lodash.cloneDeep(loader.Apps)
+    apps = apps.map(item => {
+      if (item.rule && Array.isArray(item.rule)) {
+        item.rule = item.rule.map(rule => {
+          if (rule.reg) {
+            rule.reg = rule.reg.toString()
+          }
+          return rule
+        })
+      }
+      return item
+    })
+    const instructions = {}
+    // 将指令按照 file.dir 分类
+    apps.forEach(command => {
+      const { file, name, rule } = command
+      if (!instructions[file.dir]) {
+        instructions[file.dir] = {}
+      }
+
+      // 添加子类和对应的规则
+      instructions[file.dir][name] = rule.map(r => {
+        return { reg: r.reg, example: r.reg.replace(/\//g, '').replace(/\^|\$/g, '') }
+      })
+    })
+    const client = new SparkClient({
+      Domain: Cfg.Spark.Domain,
+      AppId: Cfg.Spark.AppId,
+      APISecret: Cfg.Spark.APISecret,
+      APIKey: Cfg.Spark.APIKey,
+      Temperature: 0.1,
+      MaxTokens: 2048
+    })
+
+    const command = await client.sendMessage(JSON.stringify(instructions), {
+      prompt: [
+        {
+          "role": Cfg.Spark.Domain === "general" ? "user" : "system",
+          "content": `你是一个正则表达式示例生成功能，根据给定结构中的reg正则表达式生成示例字符串，生成的示例要求可直接被正则表达式匹配，回复我的内容只包含各个项目的列表，不包含功能说明。`
+        }
+      ]
+    })
+    if (command) {
+      tempCommandList = command.text
+      this.reply(command.text)
     }
   }
 }
